27dd6c3aec431d2bfe5194c2e4996a549b9c2d22
caedbudris <caed.budris@gmail.com>
2015-11-24 19:18:29 +0000

    a few controller tests

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class MoviesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @movie = movies(:Robocop)
+  end
+  
+  test 'Should get index' do
+    get :index
+    assert_response :success
+  end
+  
+  test 'Should get new' do
+    get :new
+    assert_response :success
+  end
+  
+  test 'Should get edit' do
+    get :edit, id: @movie
+    assert_response :success
+  end
+  
+  test 'Should create a new movie' do
+    assert_difference 'Movie.count' do
+      post :create, movie: { title: 'Some Title', category: 'Action', description: 'Description' * 5 }
+    end
+    assert_redirected_to movie_path(assigns(:movie))
+  end
+  
 end
